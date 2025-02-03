7a7d9aff2a81c39dbd094dedaea1bccf0073c803
Syed Ayaan Ali <94982193+incognito-root@users.noreply.github.com>
2025-02-03 19:50:32 +0500

    basic project structure

---
+++ TablesTurn/App/AppConfig.swift
@@ -0,0 +1,8 @@
+//
+//  AppConfig.swift
+//  TablesTurn
+//
+//  Created by Ayaan Ali on 03/02/2025.
+//
+
+import Foundation
--- TablesTurn/App/TablesTurnApp.swift
+++ TablesTurn/App/TablesTurnApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TablesTurnApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           
         }
     }
 }

fe1a6c714bf0441f023c483af85432e49b37b75d
Syed Ayaan Ali <94982193+incognito-root@users.noreply.github.com>
2025-02-03 18:42:52 +0500

    Initial Commit

---
+++ TablesTurn/TablesTurnApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablesTurnApp.swift
+//  TablesTurn
+//
+//  Created by Ayaan Ali on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TablesTurnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

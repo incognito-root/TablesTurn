8f518fa0267f26f1c38c1ac55e1c3675c9b74e8e
Syed Ayaan Ali <94982193+incognito-root@users.noreply.github.com>
2025-02-08 03:10:11 +0500

    scroll view added

--- TablesTurn/Features/Home/Views/HomeView.swift
+++ TablesTurn/Features/Home/Views/HomeView.swift
@@ -24,146 +24,149 @@ struct HomeView: View {
                 )
                 .ignoresSafeArea()
                 
-                VStack(alignment: .leading, spacing: 20) {
-                    HStack(alignment: .top) {
-                        Image("LogoImage")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                        Text("Tables Turn")
-                            .font(.title2)
-                    }
-                    
-                    HStack {
-                        VStack {
-                            Text(formattedDate.day)
-                                .font(.system(size: 35))
-                                .fontWeight(.semibold)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            Text(formattedDate.date)
-                                .font(.system(size: 35))
-                                .fontWeight(.semibold)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        
-                        Spacer()
-                        
-                        VStack(spacing: 10) {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        HStack(alignment: .top) {
                             Image("LogoImage")
                                 .resizable()
-                                .frame(width: 40, height: 40)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                            
-                            HStack(spacing: 3) {
-                                Image(systemName: "location.fill")
-                                Text("Tables Turn")
-                            }
-                            .frame(maxWidth: .infinity, alignment: .trailing)
+                                .frame(width: 25, height: 25)
+                            Text("Tables Turn")
+                                .font(.title2)
                         }
-                    }
-                    
-                    HStack {
-                        VStack(alignment: .leading, spacing: 15) {
-                            HStack {
-                                Button("Today".uppercased()) {}
-                                    .buttonStyle(MainButtonStyle(
-                                        maxWidth: 80,
-                                        padding: 12,
-                                        fontSize: 13,
-                                        cornerRadius: 30,
-                                        backgroundColor: .black,
-                                        foregroundColor: .white,
-                                        borderColor: .black,
-                                        borderWidth: 2
-                                    ))
+                        
+                        HStack {
+                            VStack {
+                                Text(formattedDate.day)
+                                    .font(.system(size: 35))
+                                    .fontWeight(.semibold)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Text(formattedDate.date)
+                                    .font(.system(size: 35))
+                                    .fontWeight(.semibold)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            
+                            Spacer()
+                            
+                            VStack(spacing: 10) {
+                                Image("LogoImage")
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
                                 
-                                Button("Calendar".uppercased()) {}
-                                    .buttonStyle(MainButtonStyle(
-                                        maxWidth: 80,
-                                        padding: 12,
-                                        fontSize: 13,
-                                        cornerRadius: 30,
-                                        backgroundColor: .clear,
-                                        foregroundColor: .white,
-                                        borderColor: .white,
-                                        borderWidth: 2
-                                    ))
+                                HStack(spacing: 3) {
+                                    Image(systemName: "location.fill")
+                                    Text("Tables Turn")
+                                }
+                                .frame(maxWidth: .infinity, alignment: .trailing)
                             }
-                            
-                            HStack {
-                                CustomTextField(
-                                    placeholder: "Search Event",
-                                    text: $viewModel.searchKey,
-                                    isSecure: false,
-                                    keyboardType: .default,
-                                    iconName: "lock",
-                                    backgroundColor: Color.inputField,
-                                    borderColor: .black,
-                                    iconColor: .black,
-                                    textColor: .black,
-                                    placeHolderColor: .black,
-                                    validation: { input in
-                                        if input.isEmpty {
-                                            return "Password cannot be empty."
+                        }
+                        
+                        HStack {
+                            VStack(alignment: .leading, spacing: 15) {
+                                HStack {
+                                    Button("Today".uppercased()) {}
+                                        .buttonStyle(MainButtonStyle(
+                                            maxWidth: 80,
+                                            padding: 12,
+                                            fontSize: 13,
+                                            cornerRadius: 30,
+                                            backgroundColor: .black,
+                                            foregroundColor: .white,
+                                            borderColor: .black,
+                                            borderWidth: 2
+                                        ))
+                                    
+                                    Button("Calendar".uppercased()) {}
+                                        .buttonStyle(MainButtonStyle(
+                                            maxWidth: 80,
+                                            padding: 12,
+                                            fontSize: 13,
+                                            cornerRadius: 30,
+                                            backgroundColor: .clear,
+                                            foregroundColor: .white,
+                                            borderColor: .white,
+                                            borderWidth: 2
+                                        ))
+                                }
+                                
+                                HStack {
+                                    CustomTextField(
+                                        placeholder: "Search Event",
+                                        text: $viewModel.searchKey,
+                                        isSecure: false,
+                                        keyboardType: .default,
+                                        iconName: "lock",
+                                        backgroundColor: Color.inputField,
+                                        borderColor: .black,
+                                        iconColor: .black,
+                                        textColor: .black,
+                                        placeHolderColor: .black,
+                                        validation: { input in
+                                            if input.isEmpty {
+                                                return "Password cannot be empty."
+                                            }
+                                            return nil
                                         }
-                                        return nil
-                                    }
-                                )
-                                .frame(maxWidth: .infinity)
+                                    )
+                                    .frame(maxWidth: .infinity)
+                                }
                             }
-                        }
-                        
-                        Spacer()
-                        
-                        VStack(spacing: 15) {
-                            Button(action: {}) {
-                                Image(systemName: "plus")
-                                    .font(.system(size: 20))
-                            }
-                            .buttonStyle(CircularButtonStyle(
-                                size: 50,
-                                foregroundColor: .black
-                            ))
                             
-                            Button(action: {}) {
-                                Image(systemName: "gear")
-                                    .font(.system(size: 20))
+                            Spacer()
+                            
+                            VStack(spacing: 15) {
+                                Button(action: {}) {
+                                    Image(systemName: "plus")
+                                        .font(.system(size: 20))
+                                }
+                                .buttonStyle(CircularButtonStyle(
+                                    size: 50,
+                                    foregroundColor: .black
+                                ))
+                                
+                                Button(action: {}) {
+                                    Image(systemName: "gear")
+                                        .font(.system(size: 20))
+                                }
+                                .buttonStyle(CircularButtonStyle(
+                                    size: 50,
+                                    foregroundColor: .black
+                                ))
                             }
-                            .buttonStyle(CircularButtonStyle(
-                                size: 50,
-                                foregroundColor: .black
-                            ))
+                            .frame(maxWidth: 50, alignment: .trailing)
                         }
-                        .frame(maxWidth: 50, alignment: .trailing)
-                    }
-                    
-                    Text("Visit ")
-                        .font(.system(size: 35)) +
-                    Text("events")
-                        .font(.system(size: 35, weight: .bold))
-                        +
-                    Text(" based on your interest")
-                        .font(.system(size: 35))
-                    
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("All")
-                                .underline()
-                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                        }
-                    }
-                    
-                    VStack {
-                        EventCard(imageUrl: URL(string: "https://plus.unsplash.com/premium_photo-1686783007953-4fcb40669dd8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGV2ZW50c3xlbnwwfHwwfHx8MA%3D%3D")!)
                         
-                        EventCard(imageUrl: URL(string: "https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")!)
+                        Text("Visit ")
+                            .font(.system(size: 35)) +
+                        Text("events")
+                            .font(.system(size: 35, weight: .bold))
+                            +
+                        Text(" based on your interest")
+                            .font(.system(size: 35))
+                        
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("All")
+                                    .underline()
+                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            }
+                        }
+                        
+                        VStack(spacing: 20) {
+                            EventCard(imageUrl: URL(string: "https://plus.unsplash.com/premium_photo-1686783007953-4fcb40669dd8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGV2ZW50c3xlbnwwfHwwfHx8MA%3D%3D")!)
+                            
+                            EventCard(imageUrl: URL(string: "https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")!)
+                        }
+                        
                     }
-                    
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .ignoresSafeArea()
+                    .padding(20)
                 }
-                .frame(maxHeight: .infinity, alignment: .top)
-                .ignoresSafeArea()
-                .padding(20)
+                .frame(maxHeight: .infinity)
             }
             .foregroundStyle(.primaryText)
             .alert(isPresented: $viewModel.showAlert) {

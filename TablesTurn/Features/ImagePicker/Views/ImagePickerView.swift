a8fb62770a2a22e01d8694349abf937d817c80aa
Syed Ayaan Ali <94982193+incognito-root@users.noreply.github.com>
2025-02-11 22:20:43 +0500

    image picker complete

--- TablesTurn/Features/ImagePicker/Views/ImagePickerView.swift
+++ TablesTurn/Features/ImagePicker/Views/ImagePickerView.swift
@@ -6,41 +6,41 @@ struct ImagePickerView: View {
     
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
-            // Display content based on the image state.
             switch viewModel.imageState {
             case .empty:
-                // Optionally, you could show a placeholder here.
                 EmptyView()
                 
             case .loading(let progress):
-                ProgressView(progress)
-                Text("Loading...")
+                VStack {
+                    ProgressView(progress)
+                    Text("Loading...")
+                }
+                .frame(height: 180)
                 
             case .success(let image):
                 image
                     .resizable()
+                    .frame(height: 180)
                     .scaledToFit()
-                    .cornerRadius(10) // Rounded corners for the image preview
+                    .cornerRadius(10)
                 
             case .failure(let errorMessage):
                 Text("Error: \(errorMessage)")
                     .foregroundColor(.red)
             }
             
-            // Show Reset button if there is an image selected, loading in progress, or error state.
             if case .empty = viewModel.imageState {
-                // When empty, show the PhotosPicker button to select an image.
                 PhotosPicker(selection: $viewModel.imageSelection, matching: .images) {
                     Text("Select Image")
                         .frame(maxWidth: .infinity)
+                        .frame(height: 180)
                         .padding()
                         .background(Color.gray.opacity(0.2))
                         .cornerRadius(10)
                 }
             } else {
-                // Otherwise, display a reset button.
                 Button(action: {
-                    viewModel.cancelLoading()  // This method resets the imageState and imageSelection
+                    viewModel.cancelLoading()
                 }) {
                     Text("Reset Image")
                         .frame(maxWidth: .infinity)
